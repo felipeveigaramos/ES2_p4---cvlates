985dcb1e48a24f3fbba4a609cd9ac50638990312
Felipe V Ramos <fveigaramos@gmail.com>
2016-07-03 17:25:32 -0300

    corrigidos problemas com a entidade
    close #11
    close #12

--- programa/src/main/java/br/edu/utfpr/cm/es2/cvlates/entity/Usuario.java
+++ programa/src/main/java/br/edu/utfpr/cm/es2/cvlates/entity/Usuario.java
@@ -17,11 +17,15 @@ public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column(length=11, unique= true, nullable=false)
+    @Column(length = 11, unique = true, nullable = false)
     private String cpf;
+    @Column(length = 100, nullable = false)
     private String nome;
+    @Column(length = 50, nullable = false)
     private String email;
+    @Column(length = 20, nullable = false)
     private String senha;
+    @Column
     private boolean ativo;
     private static final long serialVersionUID = 1L;
 
@@ -77,4 +81,75 @@ public class Usuario implements Serializable {
         this.ativo = ativo;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return nome + " " + cpf;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (ativo ? 1231 : 1237);
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (ativo != other.ativo)
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (senha == null) {
+            if (other.senha != null)
+                return false;
+        } else if (!senha.equals(other.senha))
+            return false;
+        return true;
+    }
 }
